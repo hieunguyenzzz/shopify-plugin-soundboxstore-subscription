d2e7b56cf609fd4773951c50a3335fac605d0779
ngohoanglong <ngohoanglongptit@gmail.com>
2023-12-28 18:27:32 +0700

    update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { DownloadIcon } from "lucide-react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { FieldItem } from "./components/field-item";
 import PDFViewer from "./components/pdf-viewer";
@@ -117,6 +118,77 @@ function App() {
         console.log(JSON.parse(data.signature.data))
       })
   }, [])
+  const [url, setUrl] = useState<string>()
+  console.log({ url })
+  if (url) {
+    return <>
+      <div
+        className={
+          "absolute inset-0 h-screen flex justify-center max-w-full items-center p-6 "
+        }
+      >
+        <div key={url}
+          ref={containerRef}
+          className="bg-background  gap-6 overflow-y-scroll overflow-x-hidden p-6 w-full h-full text-foreground group relative rounded-lg border-2 backdrop-blur-[2px] gradient-border-mask dark:gradient-border-mask before:pointer-events-none before:absolute before:-inset-[2px] before:rounded-lg before:p-[2px] before:[background:linear-gradient(var(--card-gradient-degrees),theme(colors.documenso.DEFAULT/70%)_5%,theme(colors.border/80%)_30%)] shadow-[0_0_0_4px_theme(colors.gray.100/70%),0_0_0_1px_theme(colors.gray.100/70%),0_0_0_0.5px_theme(colors.blue.DEFAULT/70%)] dark:shadow-[0] focus-visible:ring-ring ring-offset-background  flex-1 cursor-pointer items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 aria-disabled:pointer-events-none aria-disabled:opacity-60 min-h-[40vh]"
+        >
+          <div className="w-full min-h-full justify-center flex gap-6 mx-auto">
+            <div className="flex-1 rounded-lg max-w-4xl w-0 ">
+              <PDFViewer
+                className=" w-full overflow-hidden rounded"
+                documentData={{
+                  data: url,
+                  type: "S3_PATH",
+                  id: url,
+                }}
+                onDocumentLoad={doc => {
+                  setTimeout(() => {
+                    setDocument(doc)
+                  }, 1000)
+                }}
+              />
+
+            </div>
+            <div className="w-1/3 max-w-[400px] bg bg-gray-100 h-full rounded-lg p-6 sticky top-0">
+              <div className="flex flex-1 flex-col">
+                <div>
+                  <h3 className="text-foreground text-2xl font-semibold">
+                    Complete
+                  </h3>
+
+                  <hr className="border-border mb-8 mt-4" />
+                  <div className="relative w-full rounded-lg bg-white ">
+                    <SignaturePad
+                      className="h-44 w-full p-6"
+                      defaultValue={undefined}
+                      onChange={setImageData}
+                    />
+                  </div>
+                  <hr className="border-border mb-8 mt-4" />
+                  <div className="flex flex-col gap-4 md:flex-row">
+                    <a
+                      href={url}
+                      download={true}
+                      className="inline-flex gap-1 items-baseline justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-blue-500 text-blue-100 hover:bg-blue-600 h-11 px-8 rounded-md w-full"
+                      type="button"
+                      aria-haspopup="dialog"
+                      aria-expanded="false"
+                      aria-controls="radix-:Raijnlfff9ta:"
+                      data-state="closed"
+                    >
+                      <div>
+                        <DownloadIcon />
+                      </div>
+                      Download
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  }
   return (
     <>
       <div
@@ -182,9 +254,9 @@ function App() {
                       Cancel
                     </button>
                     <button
-                      onClick={e => {
+                      onClick={async e => {
                         e.preventDefault()
-                        fetch("https://lender-secondary-voters-mariah.trycloudflare.com/customer/signing", {
+                        const data = await fetch("https://lender-secondary-voters-mariah.trycloudflare.com/customer/signing", {
                           body: JSON.stringify({
                             id: Date.now() + '',
                             documentId: "https://cdn.shopify.com/s/files/1/0661/3034/6209/files/Brochure-Digital-Format-NB.pdf?v=1703587780",
@@ -196,7 +268,11 @@ function App() {
                             "Content-Type": "application/json"
                           },
                           method: "POST"
-                        })
+                        }).then(res => res.json())
+                        console.log('==>', data)
+                        let id = data?.response?.id
+                        const url = `https://lender-secondary-voters-mariah.trycloudflare.com/customer/signing?id=${id}`
+                        setUrl(url)
                       }}
                       className="inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-blue-500 text-blue-100 hover:bg-blue-600 h-11 px-8 rounded-md w-full"
                       type="button"
