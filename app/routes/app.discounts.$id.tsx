96841267f79c00610f2b217ca7a078bda9700d22
ngohoanglong <ngohoanglongptit@gmail.com>
2024-01-16 17:58:34 +0700

    update

--- app/routes/app.discounts.$id.tsx
+++ app/routes/app.discounts.$id.tsx
@@ -290,18 +290,17 @@ export default function SettingPage() {
             const media = <Icon source={`<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M374.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-320 320c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l320-320zM128 128A64 64 0 1 0 0 128a64 64 0 1 0 128 0zM384 384a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"></path></svg>`
             }>
             </Icon>;
-            let url = `/app/discounts/${id.split('/').pop()}`
-            console.log({ url })
             return (
               <ResourceItem
                 id={id}
-                url={`/app/discounts/${id.split('/').pop()}`}
+                url={"#"}
                 media={media}
                 accessibilityLabel={`View details for ${name}`}
               >
                 <Text variant="bodyMd" fontWeight="bold" as="h3">
                   {name}
                 </Text>
+                <div>{''}</div>
               </ResourceItem>
             );
           }}
